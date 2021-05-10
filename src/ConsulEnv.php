fb4fe2b1a2d801aeabd8e9cf530877f9bce49a46
keyhoang <hoangky@toprate.io>
2021-05-10 09:14:55 +0700

    refactor_code

---
+++ src/ConsulEnv.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace KeyHoang\PhpConsulEnv;
+
+class ConsulEnv
+{
+    protected static string $fileName = '.env.consul';
+    protected static string $domain;
+    protected static string $path;
+    protected static string $token;
+
+    public function ConsulEnv(string $domain, string $path, string $token)
+    {
+        self::$domain = $domain;
+        self::$path   = $path;
+        self::$token  = $token;
+    }
+
+    private function _getConsul(): array
+    {
+        $consul = new ConsulKV();
+
+        return $consul->getKV(self::$domain, self::$path, self::$token);
+    }
+
+    private function writeKV(array $consuls): string
+    {
+        $env = '';
+        foreach ($consuls as $consul) {
+            if ($consul->Value) {
+                $key   = strtoupper(str_replace('/', '_', str_replace(self::$path . "/", '', $consul->Key)));
+                $value = str_replace('"', '', base64_decode($consul->Value));
+                $env   .= $key . "=" . $value . "\n";
+            }
+        }
+
+        return $env;
+    }
+
+    public function createEnvConsulFile()
+    {
+        if (file_exists(self::$fileName)) {
+            unlink(self::$fileName);
+        }
+        $consul = $this->_getConsul();
+        $env    = $this->writeKV($consul);
+        $file   = self::$fileName;
+        $person = $env;
+        file_put_contents($file, $person, FILE_APPEND | LOCK_EX);
+    }
+}
